c311628747f17fcd45c0be3b68cc55a8c0cd9466
Gerardo Daniel García de la Garza <a00836801@tec.mx>
2025-03-03 19:46:27 -0600

    container bien

--- MainProject/MtdrSpring/backend/containerImgGenerator.bat
+++ MainProject/MtdrSpring/backend/containerImgGenerator.bat
@@ -1,6 +1,6 @@
 docker stop agilecontainer
 docker rm -f agilecontainer
 docker rmi arcproject
-mvn verify
+call mvn verify
 docker build -f Dockerfile --platform linux/amd64 -t arcproject .
 docker run -d --name agilecontainer -p 8080:8080 arcproject
