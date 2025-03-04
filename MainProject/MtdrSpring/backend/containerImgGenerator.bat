0fd4b86e4eb5c74e0538b163f19427174dcd100e
Gerardo Daniel García de la Garza <a00836801@tec.mx>
2025-03-03 19:07:04 -0600

    mejora en container image

--- MainProject/MtdrSpring/backend/containerImgGenerator.bat
+++ MainProject/MtdrSpring/backend/containerImgGenerator.bat
@@ -1,5 +1,5 @@
-docker stop agilecontainer
-docker rm -f agilecontainer
+docker stop arcproject
+docker rm -f arcproject
 docker rmi arcproject
 mvn verify
 docker build -f Dockerfile --platform linux/amd64 -t arcproject .
